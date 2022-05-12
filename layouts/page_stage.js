5742d01395813753c1b5b3d2826cb7ada25bb73b
Victor <victor@webcoda.com.au>
2022-05-12 14:22:01 +1000

    wip

--- layouts/page_stage.js
+++ layouts/page_stage.js
@@ -1,49 +1,66 @@
-import get from "lodash.get";
-import { Image, Layout, RichText, UnknownComponent } from "../components";
-import { Container, makeStyles, Typography, useTheme } from "@material-ui/core";
+import get from 'lodash.get'
+import { Image, Layout, RichText, UnknownComponent } from '../components'
+import { Container, makeStyles, Typography, useTheme } from '@material-ui/core'
+import dynamic from 'next/dynamic'
+import { Tabs, TabItemWrapper, TabItem, TabSection } from '../lib/nsw-ds-react/src/component/tabs/tabs'
+
+const ReactJson = dynamic(() => import('react-json-view'), { ssr: false })
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    paddingTop: theme.spacing(4),
-  },
-}));
+	root: {
+		paddingTop: theme.spacing(4),
+	},
+}))
 
 function SimplePage(props) {
-  const classes = useStyles();
-  const page = get(props, "data.page.item", null);
+	const classes = useStyles()
+	const page = get(props, 'data.page.item', null)
+	const syllabuses = get(props, 'data.syllabuses.items', null)
 
-  const theme = useTheme();
-  const imageSizes = `${theme.breakpoints.values.md}px`;
+	const theme = useTheme()
+	const imageSizes = `${theme.breakpoints.values.md}px`
 
-  if (!page) {
-    return (
-      <UnknownComponent>
-        Page {get(page, "elements.system.codename", null)} does not have any
-        content!
-      </UnknownComponent>
-    );
-  }
+	if (!page) {
+		return (
+			<UnknownComponent>
+				Page {get(page, 'elements.system.codename', null)} does not have any content!
+			</UnknownComponent>
+		)
+	}
 
-  const title = get(page, "elements.stage.linkedItems.0.elements.title.value", null)
-  const syllabuses = get(page, "elements.stage.linkedItems.0.elements.syllabuses.linkedItems", null)?.map(linkedItem => linkedItem.elements)
-  console.log(get(page, "elements.stage", null));
-  return (
-    <Layout {...props}>
-      <Container className={classes.root} maxWidth="md">
-        {title && <Typography variant="h1">{title}</Typography>}
+	const title = get(page, 'elements.stage.linkedItems.0.elements.title.value', null)
 
-        <ul>
-          {syllabuses.map((syl) => (
-            <li key={get(syl, "title.value", "")}>
-              {get(syl, "title.value", "")}
-            </li>
-          ))}
-        </ul>
+	return (
+		<Layout {...props}>
+			<Container className={classes.root} maxWidth="md">
+				{title && <Typography variant="h1">{title}</Typography>}
 
-        <pre>{JSON.stringify(page)}</pre>
-      </Container>
-    </Layout>
-  );
+				<Tabs>
+					<TabItemWrapper>
+						<TabItem title="Syllabus" urlHash="syllabus" />
+						<TabItem title="JSON of props" urlHash="json" />
+					</TabItemWrapper>
+					<TabSection urlHash="syllabus">
+						<ul>
+							{syllabuses.map((syllabus) => (
+								<li key={get(syllabus, 'system.id', null)}>
+									<details>
+										<summary>{syllabus.elements.title.value}</summary>
+										<div
+											dangerouslySetInnerHTML={{ __html: syllabus.elements.overview.value }}
+										></div>
+									</details>
+								</li>
+							))}
+						</ul>
+					</TabSection>
+					<TabSection urlHash="json">
+						<ReactJson src={props} collapsed />
+					</TabSection>
+				</Tabs>
+			</Container>
+		</Layout>
+	)
 }
 
-export default SimplePage;
+export default SimplePage
